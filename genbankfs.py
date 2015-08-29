bd88677308cccabf93d7933e32eb979be885b6ed
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-08-29 14:01:52 +0100

    Create folder per accession ID

--- genbankfs.py
+++ genbankfs.py
@@ -17,41 +17,77 @@ if not hasattr(__builtins__, 'bytes'):
     bytes = str
 
 class GenbankSearch(object):
-  def __init__(self, csv_filename):
-    self.columns = dict(zip(['# assembly_accession',
-                             'taxid',
-                             'organism_name'],
-                            ['accession',
-                             'taxid',
-                             'organism_name']
-                           ))
-    self.database = pd.read_csv(csv_filename, delimiter='\t')
-    for original_column, slug_column in self.columns.items():
-      self.database[slug_column+'_slug'] = map(self._slug, self.database[original_column])
+  def __init__(self, input_file):
+    self.database = pd.read_csv(input_file, delimiter='\t')
+    self.folders = ['accession',
+                    'species_taxid',
+                    'taxid',
+                    'organism_name',
+                    'genus',
+                    'species']
+    column_map = zip(['# assembly_accession',
+                       'species_taxid',
+                       'taxid',
+                       'organism_name'],
+                       self.folders)
+    for original_column, slug_column in column_map:
+      self.database[slug_column+'_slug'] = map(self._slug,
+                                               self.database[original_column])
+    self.database['genus_slug'] = map(self._get_genus,
+                                        self.database['organism_name'])
+    self.database['species_slug'] = map(self._get_species,
+                                        self.database['organism_name'])
 
   def query(self, **terms):
     relevant_terms = {key+'_slug': self._slug(value)
                         for key,value in terms.items()
-                        if key in self.columns.values()}
+                        if key in self.folders}
     query_str = " & ".join(["{} == '{}'".format(key, value) for key,value in
                        relevant_terms.items()])
     if not query_str:
       return self.database
     return self.database.query(query_str)
 
-  def list(self, column, **terms):
-    if not column in self.columns.values():
-      raise ValueError("{} not in columns".format(column))
-    return list(set(self.query(**terms)[column+'_slug']))
+  def list(self, folder, **terms):
+    if not folder in self.folders:
+      raise ValueError("{} not in folders".format(folder))
+    return list(set(self.query(**terms)[folder+'_slug']))
 
   def _slug(self, value):
-    return slugify(str(value), lower=False)
+    return slugify(str(value), lower=True)
+
+  def _get_genus(self, species_name):
+    genus, species = species_name.split(" ", 2)[:2]
+    return self._slug(genus)
+
+  def _get_species(self, species_name):
+    genus, species = species_name.split(" ", 2)[:2]
+    return self._slug("%s_%s" % (genus, species))
 
 class GenbankCache(object):
   pass
 
 class GenbankFuse(Operations):
-  pass
+  def __init__(self, searcher):
+    self.searcher = searcher
+    super(GenbankFuse, self).__init__()
+
+  def readdir(self, path, fh):
+    print path
+    if path == '/accession':
+      return ['.', '..'] + self.searcher.list('accession')
+    return ['.', '..', 'genus', 'taxid', 'species_taxid', 'accession']
+
+  def getattr(self, path, fh=None):
+    return dict(st_mode=(S_IFDIR | 365), st_nlink=2,
+                st_size=0, st_ctime=time(), st_mtime=time(),
+                st_atime=time())
 
 if __name__ == '__main__':
-  pass
+  parser = argparse.ArgumentParser()
+  parser.add_argument("assembly_details", type=argparse.FileType('r'))
+  parser.add_argument("mount_point", type=str)
+  args = parser.parse_args()
+
+  searcher = GenbankSearch(args.assembly_details)
+  fuse = FUSE(GenbankFuse(searcher), args.mount_point, foreground=True)
